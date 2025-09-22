20d974abc70e12abce2fabd0c901360624d1fea8
Valentin Lerouge <cloudd2vi@gmail.com>
2025-09-22 21:30:21 +0200

    Ajout d'une gestion des erreurs lors de l'exécution du script d'enrichissement

--- src/enrich/index.ts
+++ src/enrich/index.ts
@@ -323,3 +323,11 @@ export const runEnrichment = async () => {
     await prisma.$disconnect();
   }
 };
+
+if (require.main === module) {
+  runEnrichment().catch((error) => {
+    logger.error('Enrichissement échoué: %s', (error as Error).message);
+    process.exitCode = 1;
+    void prisma.$disconnect();
+  });
+}
