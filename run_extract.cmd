97aa021fd5968158038a6059f77f1dfed2c6ab71
Valentin Lerouge <cloudd2vi@gmail.com>
2025-10-08 16:14:09 +0200

    feat: ajouter des scripts pour l'enrichissement, l'extraction et la synchronisation des données open data

---
+++ run_extract.cmd
@@ -0,0 +1,28 @@
+@echo off
+REM Change directory to the script location
+cd /d %~dp0
+
+REM Ensure Puppeteer can find the expected temp directories
+SET "LOCALAPPDATA=C:\Users\%USERNAME%\AppData\Local"
+SET "TEMP=%LOCALAPPDATA%\Temp"
+SET "TMP=%LOCALAPPDATA%\Temp"
+
+REM Resolve Node.js installation directory if NODE_PATH is not already defined
+IF NOT DEFINED NODE_PATH (
+  SET "NODE_PATH=%ProgramFiles%\nodejs"
+)
+
+REM Abort if npm.cmd cannot be located
+IF NOT EXIST "%NODE_PATH%\npm.cmd" (
+  echo [%DATE% %TIME%] npm.cmd introuvable dans "%NODE_PATH%". >> log.txt
+  exit /b 1
+)
+
+REM Ensure npm is on PATH for the current session
+SET "PATH=%NODE_PATH%;%PATH%"
+
+REM Log startup
+echo [%DATE% %TIME%] Démarrage du scraper CPF >> log.txt
+
+REM Run the extraction process and append output to the log
+"%NODE_PATH%\npm.cmd" run extract >> log.txt 2>&1
